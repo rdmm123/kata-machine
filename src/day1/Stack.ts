f4c9fad8e1746e4f129b602eb8f8c7d4121167df
Robert Martinez <rdmarenco@uninorte.edu.co>
2024-10-19 00:45:27 -0300

    stack implementation

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,37 @@
 export default class Stack<T> {
     public length: number;
-
-    
+    private tail?: Node<T>
+    private head?: Node<T>
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
+        const newNode = {value: item} as Node<T>;
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+    }
 
-}
     pop(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        const head = this.head;
+        this.head = this.head.next;
+        this.length--;
+        return head?.value;
+    }
+
     peek(): T | undefined {
-
+        return this.head?.value;
+    }
 }
+
+type Node<T> = {
+    value: T,
+    next?: Node<T>
 }
